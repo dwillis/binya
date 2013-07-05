dbd248209c09a82c7c807aab1327f090221088bf
Derek Willis <dwillis@gmail.com>
2013-07-04 22:46:23 -0400

    added participants yml and base participant code

---
+++ lib/binya/participant.rb
@@ -0,0 +1,32 @@
+require 'yaml'
+require 'open-uri'
+module Binya
+  
+  class Participant
+
+  	attr_accessor :fomc_id, :name, :title, :term_start, :term_end, :rss_url, :img_url
+
+  	def initialize(params={})
+      params.each_pair do |k,v|
+       instance_variable_set("@#{k}", v)
+      end
+    end
+
+    def self.create(params={})
+    	self.new :fomc_id => params["fomcspeak"],
+    		:name => params["name"],
+    		:title => params["title"],
+    		:term_start => params["term_start"],
+    		:term_end => params["term_end"],
+    		:rss_url => params["rss_url"],
+    		:img_url => params["img_url"]
+    end
+
+    def self.load_all
+    	participants = YAML.load_file('participants.yml')
+    	participants.map{|p| Participant.create(p)}
+    end
+
+
+  end
+end
